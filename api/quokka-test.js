52a9c8e09f574be2a5c01162154df81075c2fd8d
Amy Kapernick <amy@kapers.dev>
2020-08-24 14:04:23 +0800

    updating code

--- api/quokka-test.js
+++ api/quokka-test.js
@@ -9,42 +9,28 @@ iteration = process.env.ITERATION,
 credentials = new ApiKeyCredentials({ inHeader: {"Prediction-key": key } })
 predictor = new PredictionAPIClient(credentials, endpoint)
 
-console.log({key, endpoint})
-
 const customVision = async (image) => {
-	context.log('test 2 1')
-	const results = await predictor.classifyImageUrl(projectId, iteration, {url: image}),
-	outcome = quokkaTest(results)
-
-	return outcome
-}
-
-const customVisionBinary = async (image) => {
-	const results = await predictor.classifyImage(projectId, iteration, image),
+	const results = await predictor.classifyImageUrl(projectId, iteration, { url: image }),
 	outcome = quokkaTest(results)
 
 	return outcome
 }
 
 const quokkaTest = (results) => {
-	let outcome = {
-		negative: null,
-		quokka: null
-	}
+	let outcome = []
 
 	results.predictions.forEach(tag => {
-		if(tag.tagName == 'Negative') {
-			outcome.negative = tag.probability
-		}
-		else if (tag.tagName == 'Quokka') {
-			outcome.quokka = tag.probability
+		if (tag.tagName == 'Negative') {
+			outcome[0] = tag.probability
+		} else if (tag.tagName == 'Quokka') {
+			outcome[1] = tag.probability
 		}
 	})
 
 	return outcome
 }
 
+
 module.exports = {
-	customVision,
-	customVisionBinary
+	customVision
 }
--- api/whatsapp/index.js
+++ api/whatsapp/index.js
@@ -1,85 +1,53 @@
 require('dotenv').config()
 
-module.exports = async function (context, req) {
-    context.log('test 1')
-    const quokkaTest = require('../quokka-test').customVision,
-       quokkaBot = require('../quokkabot').message
+const quokkaTest = require('../quokka-test'),
+customVision = quokkaTest.customVision,
+quokkaBot = require('../quokkabot')
 
-        context.log('test 2')
+const whatsappReply = (outcome) => {
+    let message,
+    photo = Math.floor(Math.random() * 12),
+    quokka = `${(outcome[1] * 100).toFixed(2)}%`,
+        notQuokka = `${(outcome[0] * 100).toFixed(2)}%`
+        
+    if (outcome[0] > outcome[1]) {
+        message = `Sorry, doesn't look like that's a quokka 😢
+        \nQuokka: ${quokka}, Not Quokka: ${notQuokka}
+        \nThat's pretty sad though, so here's a quokka`
+    } else {
+        message = `Yep, that looks like a quokka!
+        \nQuokka: ${quokka}, Not Quokka: ${notQuokka}`
+    }
 
-        const whatsappReply = (outcome) => {
-            let message,
-            quokka = `${(outcome.quokka * 100).toFixed(2)}%`,
-            notQuokka = `${(outcome.negative * 100).toFixed(2)}%`
+    return {message: message, photo: photo}
+}
 
-            if(outcome.quokka > outcome.negative) {
-                message = `Yep, that looks like a quokka!
-                    \nQuokka: ${quokka}, Not Quokka: ${notQuokka}`
-            }
-            else {
-                message = `Sorry, doesn't look like that's a quokka 😢
-                    \nQuokka: ${quokka}, Not Quokka: ${notQuokka}`
-            }
 
-            return message
-        }
-
-        context.log('test 3')
-
+module.exports = async function (context) {
     const qs = require('querystring'),
     MessagingResponse = require('twilio').twiml.MessagingResponse,
-    client = require('twilio')(
-        process.env.TWILIO_API_KEY,
-        process.env.TWILIO_API_SECRET,
-        {accountSid: process.env.ACCOUNT_SID}
-    ),
     twiml = new MessagingResponse(),
     message = twiml.message(),
     body = qs.parse(context.req.body),
     text = body.Body,
-    image = body.NumMedia && body.MediaUrl0,
-    service = client.sync.services(process.env.TWILIO_SYNC_SERVICE_SID)
-
-    context.log('test 4')
+    image = body.NumMedia && body.MediaUrl0
 
     if(image) {
-        context.log('test 5')
-        const results = await quokkaTest(image),
+        const results = await customVision(image),
         reply = whatsappReply(results)
+        
+        message.body(reply.message)
 
-        message.body(reply)
-
-        service.syncLists('pastResults').syncListItems.create({
-            data: {
-                image: image,
-                results: results
-            }
-        }).catch(console.error)
-
-        context.log('test 6')
+        if(reply.photo) {
+            message.media(`https://quokkas.amyskapers.dev/img/quokka_(${reply.photo}).jpg`)
+        }
     }
     else {
-        const results = quokkaBot(text)
+        const results = quokkaBot.message(text)
 
         message.body(results.body)
         message.media(results.media)
-
-        if(results.error) {
-            client.messages.create({
-                from: 'whatsapp:+61488845130',
-                body: `A new error has been lodged ⚠\n${text}`,
-                to: `whatsapp:${process.env.MOBILE}`
-            })
-        }
-
-        service.documents('image').update({
-            data: {
-                image: results.media
-            }
-        }).catch(console.error)
     }
-
-    context.log('test 9')
 
     context.done(null, {
         status: 200,
