8df6381f6f09675be4df48e7dbd5b71474b4ac12
Divya Sri Darimisetti <56034618+irsayvid@users.noreply.github.com>
2020-10-01 10:12:46 +0530

    Add few more facts to fact.js

--- resources/data/facts.js
+++ resources/data/facts.js
@@ -1,5 +1,7 @@
 [
 	`Quokkas can survive a long time without food and water`,
 	`Quokkas are peceful with each other`,
-	`They look cute, but they're pretty annoying.\nQuokkas are known to wander inter restaurants nad shops and have to be shooed away!`
+	`They look cute, but they're pretty annoying.\nQuokkas are known to wander inter restaurants nad shops and have to be shooed away!`,
+	`quokkas can climb trees if they need to – with heights of up to 2 metres! Mainly, they do it to get food.`,
+	`Like their kangaroo relatives, quokkas move around by hopping and bounding, or moving forward using their hands.`
 ]
