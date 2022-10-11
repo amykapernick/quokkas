47173510ddf4bdb56fa8958692fa2a94b2cccfc0
Amy Kapernick <amy@kapers.dev>
2022-10-11 14:03:29 +0800

    initial bot

--- api/whatsapp/index.js
+++ api/whatsapp/index.js
@@ -1,53 +1,13 @@
 require('dotenv').config()
 
 const twilio = require('twilio')
-const customVision = require('../utils/quokkaTest')
-const quokkaBot = require('../utils/quokkabot')
-const quokkaResults = require('../utils/quokkaResults')
-const { updateResults, updateImage } = require('../utils/sync')
-
 
 module.exports = async function (context) {
-    const qs = require('querystring')
     const MessagingResponse = twilio.twiml.MessagingResponse
     const twiml = new MessagingResponse()
     const message = twiml.message()
-    const body = qs.parse(context.req.body)
-    const text = body.Body
-    const image = body.NumMedia && body.MediaUrl0
 
-    if (image) {
-        const results = await customVision({image})
-        const reply = quokkaResults({
-            results,
-        })
-
-        message.body(reply.message)
-        
-        if (reply?.photo) {
-            message.media(reply.photo)
-
-            updateImage({
-                image: reply.photo
-            })
-        }
-
-        updateResults({
-            image,
-            results
-        })
-    }
-    else {
-        const reply = quokkaBot({
-            text,
-            version: 'plain'
-        })
-
-        message.body(reply.body)
-        message.media(reply.media)
-    }
-
-    console.log({message})
+    message.body("Welcome to Quokkabot")
 
     context.done(null, {
         status: 200,
