9f595a1614acf205a85345aa7977891b95990b21
Amy Kapernick <amy@kapers.dev>
2022-10-12 14:33:29 +0800

    show custom vision results

--- api/whatsapp/index.js
+++ api/whatsapp/index.js
@@ -1,6 +1,18 @@
+require('dotenv').config()
+
 const twilio = require('twilio')
+const { PredictionAPIClient } = require('@azure/cognitiveservices-customvision-prediction')
+const { ApiKeyCredentials } = require('@azure/ms-rest-js')
 
 const { randomImage, quokkas, notQuokkas } = require('../_data/photos')
+
+const key = process.env.API_KEY
+const endpoint = process.env.ENDPOINT
+const projectId = process.env.PROJECT_ID
+const iteration = process.env.ITERATION
+
+const credentials = new ApiKeyCredentials({ inHeader: { "Prediction-key": key } })
+const predictor = new PredictionAPIClient(credentials, endpoint)
 
 module.exports = async function (context) {
     const MessagingResponse = twilio.twiml.MessagingResponse
@@ -8,15 +20,24 @@ module.exports = async function (context) {
     const message = twiml.message()
     const msgParams = new URLSearchParams(context.req.body)
     const msgText = msgParams.get('Body')
+    const image = msgParams.get('MediaUrl0')
+    let results = false
 
-    if(RegExp('quokka', 'i').test(msgText)) {
-        message.body('This is a quokka')
-        message.media(`https://quokkas.amyskapers.dev/img/quokkas/${randomImage(quokkas).slug}`)
+    if(image) {
+        results = await predictor.classifyImageUrl(projectId, iteration, { url: image })
     }
     else {
-        message.body('This is not a quokka')
-        message.media(`https://quokkas.amyskapers.dev/img/not_quokkas/${randomImage(notQuokkas).slug}`)
+        if(RegExp('quokka', 'i').test(msgText)) {
+            message.body('This is a quokka')
+            message.media(`https://quokkas.amyskapers.dev/img/quokkas/${randomImage(quokkas).slug}`)
+        }
+        else {
+            message.body('This is not a quokka')
+            message.media(`https://quokkas.amyskapers.dev/img/not_quokkas/${randomImage(notQuokkas).slug}`)
+        }
     }
+
+    context.log({results: JSON.stringify(results)})
 
     context.done(null, {
         status: 200,
