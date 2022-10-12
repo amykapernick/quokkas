a81bee5e7bc003979fff4ffd843709ae6e87ca27
Amy Kapernick <amy@kapers.dev>
2022-10-12 14:12:57 +0800

    have quokkabot working

--- api/whatsapp/index.js
+++ api/whatsapp/index.js
@@ -1,13 +1,22 @@
-require('dotenv').config()
-
 const twilio = require('twilio')
+
+const { randomImage, quokkas, notQuokkas } = require('../_data/photos')
 
 module.exports = async function (context) {
     const MessagingResponse = twilio.twiml.MessagingResponse
     const twiml = new MessagingResponse()
     const message = twiml.message()
+    const msgParams = new URLSearchParams(context.req.body)
+    const msgText = msgParams.get('Body')
 
-    message.body("Welcome to Quokkabot")
+    if(RegExp('quokka', 'i').test(msgText)) {
+        message.body('This is a quokka')
+        message.media(`https://quokkas.amyskapers.dev/img/quokkas/${randomImage(quokkas).slug}`)
+    }
+    else {
+        message.body('This is not a quokka')
+        message.media(`https://quokkas.amyskapers.dev/img/not_quokkas/${randomImage(notQuokkas).slug}`)
+    }
 
     context.done(null, {
         status: 200,
